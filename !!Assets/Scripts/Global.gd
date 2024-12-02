cf7d2a92203a9d8a76c39fdcefac833e83426b14
Coeliman <coeliman24@gmail.com>
2024-12-01 20:12:31 -0700

    Renaming stuff & Camera
    Reorganized some stuff, and did a lot of messing around to see if I could get a "mario camera", but its not working well. Will try to take some time to do the math and learn godots coordinate system when I get the chance

--- !!Assets/Scripts/Global.gd
+++ !!Assets/Scripts/Global.gd
@@ -1,3 +1,7 @@
 extends Node
 # THIS SCRIPT IS ONLY FOR GLOBAL VARIABLES
 var Players = 2.0
+var pf_Player1Character = 0
+var pf_Player2Character = 0
+var pf_Player3Character = 0
+var pf_Player4Character = 0
--- !!Assets/Scripts/platforming_attached.gd
+++ !!Assets/Scripts/platforming_attached.gd
@@ -1,21 +1,43 @@
 extends Node2D
 var pcount = Global.Players
+var avg_pos = Vector2(0,0)
+var avg_maxy = Vector2(0,0)
+var avg_miny = Vector2(0,0)
+var avg_y = Vector2(0,0)
+var avg_maxx = Vector2(0,0)
+var avg_minx = Vector2(0,0)
+var avg_x = Vector2(0,0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var p1char = load("res://!!Assets/Characters/char_1_pl.tscn").instantiate()
+	get_tree().current_scene.add_child(p1char)
+	p1char.global_position = Vector2(-100,0)
+	Global.pf_Player1Character = p1char
+	# loads 2 char
 	var p2char = load("res://!!Assets/Characters/char_2_pl.tscn").instantiate()
 	get_tree().current_scene.add_child(p2char)
 	p2char.global_position = Vector2(-250,0)
+	Global.pf_Player2Character = p2char
 	if pcount == 3 or pcount == 4:
 		var p3char = load("res://!!Assets/Characters/char_3_pl.tscn").instantiate()
 		get_tree().current_scene.add_child(p3char)
 		p3char.global_position = Vector2(0,0)
+		Global.pf_Player3Character = p3char
 		print("p3loaded")
 	if pcount == 4:
 		var p4char = load("res://!!Assets/Characters/char_4_pl.tscn").instantiate()
 		get_tree().current_scene.add_child(p4char)
 		p4char.global_position = Vector2(100,0)
+		Global.pf_Player4Character = p4char
 		print("p4loaded")
 	
-	
+func _process(delta: float) -> void:
+	if pcount ==2:
+		pass
+	if pcount ==3:
+		pass
+	if pcount ==4:
+		pass
+
 	
