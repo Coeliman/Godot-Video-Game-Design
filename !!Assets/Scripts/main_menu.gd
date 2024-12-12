98b058bf46c627c45c2799085f2bff5f5a141769
Seraphell <bremien002@gmail.com>
2024-12-12 09:51:32 -0700

    made shader changes
    added color changer for diferentiating between player, will ass UI to allow players to customize character color.

--- !!Assets/Scripts/main_menu.gd
+++ !!Assets/Scripts/main_menu.gd
@@ -2,18 +2,15 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass # Replace with function body
 
 func _on_start_pressed() -> void:
 	var lvlslc = load("res://!!Build/0.1.3/Menus/Level Select.tscn").instantiate()
 	get_tree().current_scene.add_child(lvlslc)
 
-
 func _on_options_pressed() -> void:
 	var options = load("res://!!Build/0.1.3/Menus/Options.tscn").instantiate()
 	get_tree().current_scene.add_child(options)
-
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
