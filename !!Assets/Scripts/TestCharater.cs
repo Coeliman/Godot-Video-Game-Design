2a45a0064477051c90efa65d7d6acca3ac92b8f5
Seraphell <bremien002@gmail.com>
2025-01-03 17:23:51 -0700

    made changes to ragdoll and collisions
    enhanced ragdooll, added limb collisions. both dont work terribly well right now.

--- !!Assets/Scripts/TestCharater.cs
+++ !!Assets/Scripts/TestCharater.cs
@@ -3,8 +3,8 @@ using System;
 
 public partial class TestCharater : CharacterBody2D
 {
-	public const float maxSpeed = 300.0f;
-	public const float accel = 1500.0f;
+	public const float maxSpeed = 176.0f;
+	public const float accel = 1250.0f;
 
     //Drag vars
     private const float DragCoefficient = 0.1f;
@@ -18,6 +18,10 @@ public partial class TestCharater : CharacterBody2D
     private Node2D leftArm;
     private Node2D rightLeg;
     private Node2D leftLeg;
+    private Bone2D rightShoulder;
+    private Bone2D leftShoulder;
+    private Bone2D rightHip;
+    private Bone2D leftHip;
 
     public override void _Ready()
     {
@@ -26,6 +30,10 @@ public partial class TestCharater : CharacterBody2D
         leftArm = GetNode<Node2D>("Character Container/IK Nodes/IKLArm");
         rightLeg = GetNode<Node2D>("Character Container/IK Nodes/IKRLeg");
         leftLeg = GetNode<Node2D>("Character Container/IK Nodes/IKLLeg");
+        rightShoulder = GetNode<Bone2D>("Character Container/Bones/Pelvis/Chest/Right_Shoulder");
+        leftShoulder = GetNode<Bone2D>("Character Container/Bones/Pelvis/Chest/Left_Shoulder");
+        rightHip = GetNode<Bone2D>("Character Container/Bones/Pelvis/Right_Hip");
+        leftHip = GetNode<Bone2D>("Character Container/Bones/Pelvis/Left_Hip");
     }
 
     public override void _PhysicsProcess(double delta)
@@ -71,17 +79,15 @@ public partial class TestCharater : CharacterBody2D
         //MoveAndSlide();
 
         Vector2 dragVector = -velocity * DragCoefficient;
-        ApplyDragToIKTarget(rightArm, dragVector, (float)delta);
-        ApplyDragToIKTarget(leftArm, dragVector, (float)delta);
-        ApplyDragToIKTarget(rightLeg, dragVector, (float)delta);
-        ApplyDragToIKTarget(leftLeg, dragVector, (float)delta);
+        ApplyDragToIKTarget(rightArm, dragVector, (float)delta, 0);
+        ApplyDragToIKTarget(leftArm, dragVector, (float)delta, 1);
+        ApplyDragToIKTarget(rightLeg, dragVector, (float)delta, 2);
+        ApplyDragToIKTarget(leftLeg, dragVector, (float)delta, 3);
     }
-    private void ApplyDragToIKTarget(Node2D ikTarget, Vector2 dragVector, float delta)
+    private void ApplyDragToIKTarget(Node2D ikTarget, Vector2 dragVector, float delta, int num)
     {
         // Current position of the IK target
         Vector2 currentPosition = ikTarget.GlobalPosition;
-
-        Vector2 origin = GlobalPosition;
 
         // Calculate the new position based on drag
         Vector2 targetPosition = currentPosition + dragVector.Normalized() * IKDragFactor;
@@ -89,12 +95,43 @@ public partial class TestCharater : CharacterBody2D
         // Smoothly interpolate to the new position
         Vector2 newPosition = currentPosition.Lerp(targetPosition, DragSmoothness);
 
+        Vector2 origin = GlobalPosition;
+        switch (num)
+        {
+            case 0:
+                origin = rightShoulder.GlobalPosition;
+                /*
+                float angle = rightShoulder.GetBoneAngle();
+
+                float minAngle = Mathf.DegToRad(90);
+                float maxAngle = Mathf.DegToRad(270);
+
+                angle = Mathf.Clamp(angle, minAngle, maxAngle);
+
+                rightShoulder.SetBoneAngle(angle);
+                */
+                break;
+
+            case 1:
+                origin = leftShoulder.GlobalPosition;
+                break;
+
+            case 2:
+                origin = rightHip.GlobalPosition;
+                break;
+
+            case 3:
+                origin = leftHip.GlobalPosition;
+                break;
+        }
+
         if (newPosition.DistanceTo(origin) > MaxIKDistance)
         {
             newPosition = origin + (newPosition - origin).Normalized() * MaxIKDistance;
         }
 
+
         // Update the IK target position
         ikTarget.GlobalPosition = newPosition;
-}
+    }
 }
