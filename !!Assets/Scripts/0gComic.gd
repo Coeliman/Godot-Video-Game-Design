af3a4d5fc3387ce2f756ca702248a75a8d6892c5
Veracles <Tallerthanthou321@gmail.com>
2025-01-11 13:45:43 -0700

    Maps

--- !!Assets/Scripts/0gComic.gd
+++ !!Assets/Scripts/0gComic.gd
@@ -3,4 +3,4 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _on_continue_pressed() -> void:
-	get_tree().change_scene_to_file("res://!!Build/0.1.3/Levels/Lvl 1.tscn")
+	get_tree().change_scene_to_file("res://!!Build/0.1.3/Levels/Tutorial.tscn")
