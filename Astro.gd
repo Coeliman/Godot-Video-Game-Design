ea3c205014f9476b3510c337e791420644a5bbcf
Seraphell <bremien002@gmail.com>
2025-01-14 10:56:24 -0700

    Fixed all ragdoll bugs with the new GD script character code
    fixed bugs with ragdoll system placing the ik targets incorrectly, hopefully it should work now if not lmk

--- Astro.gd
+++ Astro.gd
@@ -22,7 +22,7 @@ var leftHip:Bone2D
 
 func _ready() -> void:
 
-	bones =$"Character Container/Bones"
+	bones = $"Character Container/Bones"
 	rightArm = $"Character Container/IK Nodes/IKRArm"
 	leftArm = $"Character Container/IK Nodes/IKLArm"
 	rightLeg = $"Character Container/IK Nodes/IKRLeg"
@@ -30,39 +30,39 @@ func _ready() -> void:
 	rightShoulder =$"Character Container/Bones/Pelvis/Chest/Right_Shoulder"
 	leftShoulder =$"Character Container/Bones/Pelvis/Chest/Left_Shoulder"
 	rightHip = $"Character Container/Bones/Pelvis/Right_Hip"
-	leftHip =$"Character Container/Bones/Pelvis/Left_Hip"
+	leftHip = $"Character Container/Bones/Pelvis/Left_Hip"
 
 func _process(_delta: float) -> void:
 	var v:float = 0.0
 	var direction:Vector2 = Input.get_vector("p1_left","p1_right","p1_up","p1_down")
-	var velo: Vector2 = self.velocity
+	var velo: Vector2 = velocity
 	
 	if direction != Vector2.ZERO:
 		if (maxSpeed - v) > accel:
 			v += accel
-			velo += direction *v* _delta
+			velo += direction * v * _delta
 		else:
 			v=maxSpeed
-			velo+= direction*v*_delta
+			velo+= direction * v * _delta
 			
 	move_and_slide()
 	
 	if is_on_wall():
 		if get_wall_normal() != Vector2.ZERO:
 			if (abs(get_wall_normal().x)> abs(get_wall_normal().y)):
-				velocity.x = 0
+				velo.x = 0
 			else:
-				velocity.y = 0
+				velo.y = 0
 			
-	var collision = move_and_collide(velocity * _delta)
+	var collision = move_and_collide(velo * _delta)
 
 	if direction == Vector2.ZERO:
 		if collision != null:
-			if call_deferred("self.position.x")== self.position.x:
+			if call_deferred("position.x") == position.x:
 				velo.x = 0
-			if call_deferred("self.position.y")== self.position.y:
+			if call_deferred("position.y") == position.y:
 				velo.y = 0
-	self.velocity = velo
+	velocity = velo
 	
 	var dragVector:Vector2 = -velo * drag
 	_ApplyDragToIKTarget(rightArm,dragVector,_delta,0)
@@ -106,11 +106,10 @@ func _ApplyDragToIKTarget(ikTarget:Node2D,dragVector:Vector2,delta:float,num:int
 	if newPosition.distance_to(orgin) > MaxIKDistance:
 		newPosition = orgin + (newPosition-orgin).normalized() * MaxIKDistance
 	elif newPosition.distance_to(orgin) <MinIKDistance:
-			newPosition = orgin + (newPosition - orgin).normalized() * MinIKDistance
+		newPosition = orgin + (newPosition - orgin).normalized() * MinIKDistance
 	
 	if bone.rotation <= deg_to_rad(maxAngle) and bone.rotation >= deg_to_rad(minAngle):
 		ikTarget.global_position = newPosition
-		
 	else:
 		match(num):
 			0:
@@ -121,7 +120,5 @@ func _ApplyDragToIKTarget(ikTarget:Node2D,dragVector:Vector2,delta:float,num:int
 				newPosition = newPosition + Vector2(3.0,3.0)
 			3:
 				newPosition = newPosition + Vector2(-3.0,3.0)
-			
-			
 		ikTarget.global_position = newPosition
 	
