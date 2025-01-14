01f80f61ce29916b3e3e79a4954083bab809e870
Elijah <coeliman24@gmail.com>
2025-01-14 08:30:46 -0700

    Level 1, Tutorial, and Level 2 buttons are in

--- !!Assets/Scripts/lvl_select.gd
+++ !!Assets/Scripts/lvl_select.gd
@@ -3,14 +3,14 @@ extends Node
 
 
 
-func platformer_pressed() -> void:
+func platformer_pressed() -> void: #tutorial
 	queue_free()
 	# loads comic, make sure to change it to the comic scene you want
 	var Comic = load("res://!!Build/0.1.3/Menus/Comics/PlatformerComic.tscn").instantiate()
 	get_tree().current_scene.add_child(Comic)
 
 
-func zerog_pressed() -> void:
+func zerog_pressed() -> void: #level1
 	queue_free()
 	var Comic = load("res://!!Build/0.1.3/Menus/Comics/Comic Example.tscn").instantiate()
 	get_tree().current_scene.add_child(Comic)
@@ -18,3 +18,10 @@ func zerog_pressed() -> void:
 
 func _close_pressed() -> void:
 	queue_free()
+
+
+func _on_button_3_pressed() -> void: #level2
+	queue_free()
+	# loads comic, make sure to change it to the comic scene you want
+	var Comic = load("res://!!Build/0.1.3/Menus/Comics/level2comic.tscn").instantiate()
+	get_tree().current_scene.add_child(Comic)
